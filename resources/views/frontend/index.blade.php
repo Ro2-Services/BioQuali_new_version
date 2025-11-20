48dd1be9709ddbc648026710bd120902f9206947
Ro2-Services <okericardo31@gmail.com>
2025-11-20 22:35:13 +0300

    modification

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BioQuali | Laboratoire de Biotechnologie & Analyses Médicales - Bénin</title>
+    <title>BioQuali | Laboratoire de Biotechnologie & Analyses Médicales</title>
     
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
